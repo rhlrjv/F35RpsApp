4e5afc3897181c3d9165dc53ffc641e49bacab8e
Rahul Rajeev <rahul@rahulrajeev.co>
2018-07-19 19:24:34 -0700

    Remove trailing semi-colons

--- rps/app/rps.js
+++ rps/app/rps.js
@@ -5,26 +5,26 @@ function PlayRoundRequest(throw1, throw2, roundObserver) {
         } else if (tie()) {
             roundObserver.tie()
         } else if (p1Wins()) {
-            roundObserver.p1_wins();
+            roundObserver.p1_wins()
         } else {
-            roundObserver.p2_wins();
+            roundObserver.p2_wins()
         }
-    };
+    }
 
     const invalid = () => {
         return !VALID_THROWS.includes(throw1) ||
             !VALID_THROWS.includes(throw2)
-    };
+    }
 
     const tie = () => {
         return throw1 === throw2
-    };
+    }
 
     const p1Wins = () => {
         return throw1 === THROW.rock && throw2 === THROW.scissors ||
             throw1 === THROW.scissors && throw2 === THROW.paper ||
             throw1 === THROW.paper && throw2 === THROW.rock
-    };
+    }
 
     const VALID_THROWS = [THROW.rock, THROW.paper, THROW.scissors]
 }
@@ -39,6 +39,6 @@ const THROW = {
     rock: 'rock',
     paper: 'paper',
     scissors: 'scissors'
-};
+}
 
-module.exports = { RPS, THROW }
+module.exports = {RPS, THROW}
--- rps/spec/PlaySpec.js
+++ rps/spec/PlaySpec.js
@@ -1,95 +1,95 @@
 const {RPS, THROW} = require('../app/rps')
 
-describe("RPS", () => {
-    let roundObserverSpy, game;
+describe('RPS', () => {
+    let roundObserverSpy, game
 
     beforeEach(() => {
-        game = new RPS();
-    });
+        game = new RPS()
+    })
 
-    describe("P2 Wins scenarios", () => {
+    describe('P2 Wins scenarios', () => {
         beforeEach(() => {
-            roundObserverSpy = jasmine.createSpyObj('observer', ['p2_wins']);
-        });
-
-        it("rock vs paper", () => {
-            game.playRound(THROW.rock, THROW.paper, roundObserverSpy);
-            expect(roundObserverSpy.p2_wins).toHaveBeenCalled();
-        });
-
-        it("paper vs scissors", () => {
-            game.playRound(THROW.paper, THROW.scissors, roundObserverSpy);
-            expect(roundObserverSpy.p2_wins).toHaveBeenCalled();
-        });
-
-        it("scissors vs rock", () => {
-            game.playRound(THROW.scissors, THROW.rock, roundObserverSpy);
-            expect(roundObserverSpy.p2_wins).toHaveBeenCalled();
-        });
-    });
-
-    describe("P1 Wins scenarios", () => {
-        beforeEach(() => {
-            roundObserverSpy = jasmine.createSpyObj('observer', ['p1_wins']);
-        });
-
-        it("paper vs rock", () => {
-            game.playRound(THROW.paper, THROW.rock, roundObserverSpy);
-            expect(roundObserverSpy.p1_wins).toHaveBeenCalled();
-        });
-
-        it("scissors vs paper", () => {
-            game.playRound(THROW.scissors, THROW.paper, roundObserverSpy);
-            expect(roundObserverSpy.p1_wins).toHaveBeenCalled();
-        });
-
-        it("rock vs scissors", () => {
-            game.playRound(THROW.rock, THROW.scissors, roundObserverSpy);
-            expect(roundObserverSpy.p1_wins).toHaveBeenCalled();
-        });
-    });
-
-    describe("Tie scenarios", () => {
-        beforeEach(() => {
-            roundObserverSpy = jasmine.createSpyObj('observer', ['tie']);
-        });
-
-        it("rock vs rock", () => {
-            game.playRound(THROW.rock, THROW.rock, roundObserverSpy);
-            expect(roundObserverSpy.tie).toHaveBeenCalled();
-        });
-
-        it("paper vs paper", () => {
-            game.playRound(THROW.paper, THROW.paper, roundObserverSpy);
-            expect(roundObserverSpy.tie).toHaveBeenCalled();
-        });
-
-        it("scissors vs scissors", () => {
-            game.playRound(THROW.scissors, THROW.scissors, roundObserverSpy);
-            expect(roundObserverSpy.tie).toHaveBeenCalled();
-        });
-    });
-
-    describe("Invalid scenarios", () => {
-        let invalidThrow;
-        beforeEach(() => {
-            roundObserverSpy = jasmine.createSpyObj('observer', ['invalid']);
-            invalidThrow = Math.random().toString()
-        });
-
-        it("valid vs invalid", () => {
-            game.playRound(THROW.rock, invalidThrow, roundObserverSpy);
-            expect(roundObserverSpy.invalid).toHaveBeenCalled();
-        });
-
-        it("invalid vs valid", () => {
-            game.playRound(invalidThrow, THROW.paper, roundObserverSpy);
-            expect(roundObserverSpy.invalid).toHaveBeenCalled();
-        });
-
-        it("invalid vs invalid", () => {
-            game.playRound(invalidThrow, invalidThrow, roundObserverSpy);
-            expect(roundObserverSpy.invalid).toHaveBeenCalled();
+            roundObserverSpy = jasmine.createSpyObj('observer', ['p2_wins'])
         })
-    });
-});
+
+        it('rock vs paper', () => {
+            game.playRound(THROW.rock, THROW.paper, roundObserverSpy)
+            expect(roundObserverSpy.p2_wins).toHaveBeenCalled()
+        })
+
+        it('paper vs scissors', () => {
+            game.playRound(THROW.paper, THROW.scissors, roundObserverSpy)
+            expect(roundObserverSpy.p2_wins).toHaveBeenCalled()
+        })
+
+        it('scissors vs rock', () => {
+            game.playRound(THROW.scissors, THROW.rock, roundObserverSpy)
+            expect(roundObserverSpy.p2_wins).toHaveBeenCalled()
+        })
+    })
+
+    describe('P1 Wins scenarios', () => {
+        beforeEach(() => {
+            roundObserverSpy = jasmine.createSpyObj('observer', ['p1_wins'])
+        })
+
+        it('paper vs rock', () => {
+            game.playRound(THROW.paper, THROW.rock, roundObserverSpy)
+            expect(roundObserverSpy.p1_wins).toHaveBeenCalled()
+        })
+
+        it('scissors vs paper', () => {
+            game.playRound(THROW.scissors, THROW.paper, roundObserverSpy)
+            expect(roundObserverSpy.p1_wins).toHaveBeenCalled()
+        })
+
+        it('rock vs scissors', () => {
+            game.playRound(THROW.rock, THROW.scissors, roundObserverSpy)
+            expect(roundObserverSpy.p1_wins).toHaveBeenCalled()
+        })
+    })
+
+    describe('Tie scenarios', () => {
+        beforeEach(() => {
+            roundObserverSpy = jasmine.createSpyObj('observer', ['tie'])
+        })
+
+        it('rock vs rock', () => {
+            game.playRound(THROW.rock, THROW.rock, roundObserverSpy)
+            expect(roundObserverSpy.tie).toHaveBeenCalled()
+        })
+
+        it('paper vs paper', () => {
+            game.playRound(THROW.paper, THROW.paper, roundObserverSpy)
+            expect(roundObserverSpy.tie).toHaveBeenCalled()
+        })
+
+        it('scissors vs scissors', () => {
+            game.playRound(THROW.scissors, THROW.scissors, roundObserverSpy)
+            expect(roundObserverSpy.tie).toHaveBeenCalled()
+        })
+    })
+
+    describe('Invalid scenarios', () => {
+        let invalidThrow
+        beforeEach(() => {
+            roundObserverSpy = jasmine.createSpyObj('observer', ['invalid'])
+            invalidThrow = Math.random().toString()
+        })
+
+        it('valid vs invalid', () => {
+            game.playRound(THROW.rock, invalidThrow, roundObserverSpy)
+            expect(roundObserverSpy.invalid).toHaveBeenCalled()
+        })
+
+        it('invalid vs valid', () => {
+            game.playRound(invalidThrow, THROW.paper, roundObserverSpy)
+            expect(roundObserverSpy.invalid).toHaveBeenCalled()
+        })
+
+        it('invalid vs invalid', () => {
+            game.playRound(invalidThrow, invalidThrow, roundObserverSpy)
+            expect(roundObserverSpy.invalid).toHaveBeenCalled()
+        })
+    })
+})
