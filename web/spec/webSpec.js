31dc3e5d3bfc42c0c9405ba84539f9a97ea28d79
Rahul Rajeev <rahul@rahulrajeev.co>
2018-07-20 11:31:10 -0700

    First failing webspec

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,44 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const ReactDOM = require('react-dom');
+const React = require('react');
+
+describe('PlayForm', () => {
+    let domFixture
+
+    beforeEach(function () {
+        domFixture = document.createElement('div')
+        document.body.appendChild(domFixture)
+    })
+
+    afterEach(function () {
+        domFixture.remove()
+    })
+
+    function renderPlayForm(game) {
+        ReactDOM.render(
+            <PlayForm game={game}></PlayForm>,
+            domFixture
+        )
+    }
+
+    function play() {
+        domFixture.querySelector('button').click()
+    }
+
+    it('should display invalid when RPS returns invalid', () => {
+        const alwaysInvalidGameStub = {
+            play: (t1, t2, roundObserver) => roundObserver.invalid()
+        }
+        renderPlayForm(alwaysInvalidGameStub)
+
+        play()
+
+        expect(domFixture.innerText).toContain('Invalid!')
     })
 })
+
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Play!</button>
+    }
+}
